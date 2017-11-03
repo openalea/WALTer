4eba27f83cebea619977f3c111e35db1cc995ab6
Emmanuelle Blanc <ecm.blanc@gmail.com>
2017-11-03 16:16:27 +0100

    En cas de CaribuRunError (Polygon error par exemple), le script shell relance la simulation concernee en changeant la graine d'aleatoire

--- WALTer_batch_launcher.sh
+++ WALTer_batch_launcher.sh
@@ -32,9 +32,15 @@ while read -a line
 do
 	# Si le tableau param_names est vide, on le remplit avec le contenu de la 1ere ligne
 	# continue => on passe a l'iteration suivante sans executer le reste du bloc
-#	[[ -z $param_names ]] && param_names=(${line[@]}) && identifiant="ID" && OLD_IFS="$IFS" && IFS=$'\t' && echo -e "${line[*]}""\t"$identifiant >> combi_param.csv && IFS="$OLD_IFS" && continue
-	[[ -z $param_names ]] && param_names=(${line[@]}) && identifiant="ID" && OLD_IFS="$IFS" && IFS=$'\t' && echo -ne "${line[*]}\t$identifiant\n" >> combi_param.csv && IFS="$OLD_IFS" && continue
-
+#	[[ -z $param_names ]] && param_names=(${line[@]}) && identifiant="ID" && OLD_IFS="$IFS" && IFS="   " && echo -e "${line[*]}""\t"$identifiant >> combi_param.csv && IFS="$OLD_IFS" && continue
+	if [[ -z $param_names ]]; then
+		param_names=(${line[@]}) && identifiant="ID" && OLD_IFS="$IFS" && IFS=$'\t' && echo -ne "${line[*]}\t$identifiant\n" >> combi_param.csv && IFS="$OLD_IFS"
+		# numero de colonne de la variable rep		
+		col_rep=`tr '\t' '\n' < combi_param.csv | grep -n "^rep$" | cut -d":" -f1`
+		# 1er indice du tableau line = zero
+		((col_rep--))
+		continue
+	fi
 	i=0 # indice des tableaux
 	
 	# tous les parametres pour une simulation (vide) 
@@ -52,20 +58,42 @@ do
 	echo -e "\033[31m$script_params\033[0m"
 		
 	# suffixe pour des noms de fichiers de sortie standard et d'erreur
-	#suffixe="`echo ${line[@]} | tr ' ' '_'`.txt"
-
-	identifiant=`cat ID_simul.txt`
-	suffixe=$identifiant
-
+	suffixe="`echo ${line[@]} | tr ' ' '_'`.txt"
+	
 	# lancement d'une simulation (script python avec tous ses parametres)
 	#echo "$script_py $script_params >>Outs/out.$suffixe 2>Errors/err.$suffixe"
 	date | tee Times/time.$suffixe
 	{ time $script_py $script_params >>Outs/out.$suffixe 2>Errors/err.$suffixe ; } 2>&1 | tee -a Times/time.$suffixe
-
+		
+	identifiant=`cat ID_simul.txt`
+	
+	# numero de la rep concernee
+	rep=${line[$col_rep]}	
+	
+	try_nb=0; th=15
+	# tester la presence du fichier error_caribu.txt
+	while [[ -f output/$identifiant/error_caribu.txt && $try_nb -le $th ]]
+	do
+		
+		# on redefinit la chaine de parametres avec la nouvelle valeur de rep (graine)
+		#echo "script_params avant: $script_params"
+		script_params=${script_params/rep=$rep/rep=$((++rep))}
+		#echo "script_params apres: $script_params"
+		
+		# nettoyage des fichiers dans Times, Outs et Errors et du dossier identifiant
+		rm -r output/$identifiant Outs/out.$suffixe Errors/err.$suffixe Times/time.$suffixe
+				
+		# relance du bousin avec la nouvelle graine
+		{ time $script_py $script_params >>Outs/out.$suffixe 2>Errors/err.$suffixe ; } 2>&1 | tee -a Times/time.$suffixe
+		identifiant=`cat ID_simul.txt`
+		((try_nb++))
+	done
+	
+	echo "Victoire, j'ai trouvé une graine suffisamment satisfaisante: $rep, car l'autre d'avant puait du fion => ${line[$col_rep]}"
 	OLD_IFS="$IFS"
-	IFS=$'\t'
+	IFS=$'\t'	
 	#echo -e "${line[*]}""\t"$identifiant >> combi_param.csv
-	echo -ne "${line[*]}\t$identifiant\n" >> combi_param.csv
+	echo -ne "${line[*]}\t$identifiant\n" >> combi_param.csv	
 	IFS="$OLD_IFS"
 	
 	#./WALTer_launcher-v1-17_dev.py densite=$a tillering_prob_Maxwell=$b LAIc_Maxwell=$c PARseuil=$d expe_related=$e rep=$r CARIBU_state=$CARIBU >>out.$a.$b.$c.$d.$e.$r.txt 2>err.$a.$b.$c.$d.$e.$r.txt
