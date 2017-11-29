b938fb25e0529fef8cd0a97e607d17d4c35cd381
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2017-11-29 17:54:52 +0100

    setup failing test

--- test/test_main_run.py
+++ test/test_main_run.py
@@ -27,3 +27,20 @@ def test_main():
     s=lsys.sceneInterpretation(lstring)
     assert len(s) > 2
     # Viewer.display(s)
+
+
+def test_caribu():
+    lsystem_file = pj(data_access.get_data_dir(), 'WALTer.lpy')
+    lsys = Lsystem(lsystem_file,{'params': {'nb_plt_utiles': 1,
+                                  'dist_border_x':0,
+                                  'dist_border_y': 0,
+                                  'nbj': 55,
+                                  'beginning_CARIBU': 290
+
+         }})
+    assert isinstance(lsys, Lsystem)
+
+    # lsys.nbj = 10
+    # lsys.derivationLength = lsys.nbj
+    # lsys.animate()
+    lstring = lsys.iterate()
