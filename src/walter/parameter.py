d455eefbc28d93779d9c1a383f36aa2a2d101aa0
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2017-12-19 16:21:28 +0100

    add comom plant parameters

--- src/walter/parameter.py
+++ src/walter/parameter.py
@@ -14,7 +14,7 @@ def default_parameters():
     write_lscene = False
     write_lstring = False
     write_debug_PAR = False
-    #
+    # control if variability factor are applied or not
     hazard_plant_xy = True
     hazard_plant_azi = True
     hazard_axis = True
@@ -33,10 +33,84 @@ def default_parameters():
 
     # SIRIUS
     SIRIUS_state = "disabled"
+    # Final leaf number by SIRIUS
+    Tvermin = 0
+    Tverint = 8
+    Tvermax = 17
+    Lmax = 24
+    Lmin = 8
+    DLsat = 15
+    # Adjusting phyllochrone to leaf number
+    Ldecr = 2
+    Lincr = 8
+    phyllo_decr = 0.75
+    phyllo_incr = 1.25
+
+
+    # Thermal time
+    Tbase = 0.
+
+    # Phenology
+    Delta_hf = 1.3  # Delay between heading and flowering (phyllochronic unit) | old name : delta_epi_flo
+    Delta_c_GN = 30  # Duration of the critical period determining grain number (days) | old name : duration_critical_period
+    Delta_hm = 800  # Delay between heading and maturity (in degree-days) | old name : delta_epi_mat
+    Delta_lflf = 1.39  # Delay between the ligulation of the flag leaf and flowering (phyllochronic units) | old name : delta_ligflagleaf_flo
+
+    # Organ initiation, emergence and elongation
+    # Number of primordia already preformed inside the seed
+    # old name : nb_primord_seed
+    N_p_s = 4
+    # Thermal time shift between the floral transition of axes belonging to
+    #  two consecutive cohorts | old name : transiflo_shift_param
+    Psi_FT = 0
+    # Tiller emergence
+    # Delay between the initiation of a bud and the start of its activity
+    # (plastochronic unit) | old name : inactive_time_bud
+    Delta_b = 1
+    # dimensions
+    # length of the coleoptil at the emergence
+    # old name : coleoptil_length
+    l_c = 0.5
+    ED_I = 1.66  # Duration for internode extension (value extracted directly from ADEL-wheat) (phyllochronic time) | old name : gr_duration_internode
+    ED_B = 1.6  # Duration for blade extension (phyllochronic time)                                                | old name : gr_duration_blade
+    ED_FB = 1  # Duration for the extension of the flag blade(phyllochronic time)                                 | old name : gr_duration_flagblade
+    ED_S = 0.4  # Duration for sheath extension (phyllochronic time)                                                 | old name : gr_duration_sheath
+    ED_P = 2  # Duration for peduncle extension (phyllochronic time)                                                 | old name : gr_duration_peduncle
+
+    # Organ death
+    Delta_flsp = 100  # Thermal time delay between the senescence of the flag blade and the beginning of peduncle senescence (en degree-days) | old name : delta_senflagleaf_to_senped
 
     # Cessation of tillering
+    cohorte_max = 9
+    # Infinitisation du couvert pour le calcul du GAI de proximite
+    infinity_GAIp = "True"      # A METTRE EN TEXTE
     dGAIp = 1  # Maximal range of plant detection (meter)
     L_b_S = 2  # Buried length of the first sheath (in cm) | old name : L_burried
+
+    # Tiller death
+    Delta_l = 100  # Duration of radiation integration to determine the survival of a tiller (degree-day) | old name : ray_integration
+    Delta_SGtC = 600  # Thermal time between the moment a tiller stops growing and its entire removal | old name : delta_stopgr_to_cut
+    Delta_SGtR = 200  # Thermal time between the moment a tiller stops growing and the beginning of the reverse growth of leaves | old name : delta_stopgr_to_reg
+
+    # Plant geometry
+    Phi_zen_B = 40  # Angle between blade inclination and Y axis | old name : bl_incl_shift
+    Phi_azi_B = 185  # Angle between the azimuth of two consecutive blades | old name : bl_azi_shift
+    Phi_zen_T = 0  # Angle between the zenith of two consecutive tillers  | old name : till_zen_shift
+    Phi_azi_T = 40  # Angle between the azimuth of two consecutive tillers | old name : till_azi_shift
+
+    Phi_zen_E = 0  # Angle between ear inclination and Y axis | old name : ear_zen_shift
+    Phi_azi_S = 0  # Angle between seed azimuth and X axis | old name : seed_azi_shift
+    Phi_zen_S = 0  # Angle between seed inclination and Y axis | old name : seed_zen_shift
+
+    # Variability parameters (will be controled by hazard controls
+    y_position_hazard = 2
+    x_position_hazard = 3
+    z_position_hazard = 0
+    blade_incl_hazard = 5
+    blade_azi_hazard = 20
+    till_zen_hazard = 7
+    till_azi_hazard = 90
+    plant_azi_hazard = 360
 
     d = locals().copy()
     return d
@@ -77,6 +151,10 @@ def initialisation(user_parameters):
     #  par defaut.
     parameters['param_Ln_final'] = _p.get("Ln_final", 11)
 
+    # syntactic sugars
+    parameters['Delta_Reg'] = _p['Delta_SGtC'] - _p['Delta_SGtR']  # | old name : duration_reg
+
+
     #  genotypic parameters
     genotypic = genotypic_parameters()
     for what in genotypic:
@@ -85,6 +163,19 @@ def initialisation(user_parameters):
         gp = get_genotypic_parameters(g, user_parameters)
         for what in genotypic:
             parameters[what][g] = gp[what]
+
+    # hazard driver will trigger if necessary hazard in different cases:
+    # if hazard == False : hazard parameter value = 0, if hazard == True,
+    # hazard parameter value is set at the value of the parameter
+
+    for what in ('y_position_hazard','x_position_hazard','z_position_hazard'):
+        parameters[what] *= int(_p['hazard_plant_xy'])
+    for what in ('blade_incl_hazard', 'blade_azi_hazard'):
+        parameters[what] *= int(_p['hazard_organ'])
+    for what in ('till_zen_hazard', 'till_azi_hazard'):
+        parameters[what] *= int(_p['hazard_axis'])
+    parameters['plant_azi_hazard'] *= int(_p['hazard_plant_azi'])
+
 
     return parameters
 
