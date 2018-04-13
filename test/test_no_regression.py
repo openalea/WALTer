7ccb3915024e32561e6f5dd90a25098de497197d
bilalderouich <bilalderouich34@gmail.com>
2018-04-13 13:54:43 +0200

    Setup test no_regression test

--- test/test_no_regression.py
+++ test/test_no_regression.py
@@ -1,25 +1,43 @@
 # coding=utf-8
 from walter import project
-from walter.light import get_light, scene_pattern
-import pandas
+import pandas,os
+from walter.data_access import get_data_dir
+from pathlib2 import Path
+import numpy.testing as np
 
 def test_same_result_zerolight():
     #assert : verify if the same wheat field have the same results for the same chosen parameters.
-    p = project.Project(name='same')
-    # reference_file = fichier contenant le tableau de référence.
-    # reference = reference_file.read()
-    
-    params = p.csv_parameters('sim_scheme_test.csv')[0]
-    params.update(dict(nbj=165))
-    outs = p.which_outputs
-    p.which_outputs = outs
+
+    p = project.Project(name='same')  # Création du répertoire de simulation
+    params = p.csv_parameters('sim_scheme_test.csv')[0] #Récupération des parametres
+    params.update(dict(nbj=165)) #Ajout du parametre a faire varier
+    outs = p.which_outputs #Créer le fichier de sortie s'il n'est pas déjà existante.
+    p.which_outputs = outs #Affecte le fichier de sortie à la sortie du test pour sauvegarder les résultats.
     lsys, lstring = p.run(**params)
-    PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']
-    dfout = pandas.read_csv(p.dirname / 'output' / id / 'PAR_per_axes.csv', sep='\t')
-    assert (dfout == reference), " Not the same results for the same experience. "
+    with open(p.dirname/'ID_simul.txt') as f:
+        id = f.read() #Récupération de l'id
+    reference_directory = get_data_dir() + "/ref_output/" #Dossier de référence.
+    list_of_file_ref = os.listdir(reference_directory) #Listage des différents fichiers de référence.
+    reference = {} #Dictionnaire Vide
+    for i in list_of_file_ref: #Mise en place des différents résultats dans un tableau.
+        element = reference_directory + i #Récupère le chemin absolu
+        up_date = { i : pandas.read_csv(element, sep='\t')} #Récupère fichier et contenu
+        reference.update(up_date) # Dictionnaire de référence.
+        result_directory = p.dirname + '/' + 'output' + '/' + id +'/'
+        list_of_result = os.listdir(result_directory)
+        np.assert_array_equal(list_of_file_ref,list_of_result) #Vérification que les 2 listes sont égaux.
+        my_file = Path(result_directory + i)
+        if my_file.is_file() :
+            dfout = pandas.read_csv(my_file, sep='\t')
+            print(' \n Le fichier testé est : '+ i + '\n')
+            np.assert_array_equal(dfout, reference[i]) #Comparaison Référence et Simulation.
+        else :
+            print(' \n Le fichier ' + my_file + ' est inexistant ')
+
 
     p.deactivate()
     p.remove(force=True)
+
 
 
 def test_zero_light():
@@ -38,15 +56,11 @@ def test_zero_light():
     #Affecte le fichier de sortie à la sortie du test pour sauvegarder les résultats.
     lsys, lstring = p.run(**params)
     #Lance une simulation en mettant à jour les résultats de parametre pour chaque plantes simulé.
-    PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']#Récupère les variables globales du lsystem.
-    df = pandas.DataFrame(PAR_per_axes_dico)
-    PAR = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
-    assert all(PAR > 0)
     with open(p.dirname/'ID_simul.txt') as f: #Ouvre un document 'ID_simul.txt' dans le répertoire pere (repertoire courant / dans notre cas 'test/zero_light').
         id = f.read()
         #Lis le document est enregistre son écriture dans "id".
 
-    dfout = pandas.read_csv(p.dirname / 'output' / id / 'PAR_per_axes.csv', sep='\t')
+    dfout= pandas.read_csv(p.dirname / 'output' / id / 'PAR_per_axes.csv', sep='\t')
     PARout = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
     assert all(PARout > 0)
 
