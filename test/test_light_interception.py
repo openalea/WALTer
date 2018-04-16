0fb346f8ec3ada1b101164192374761d860b037b
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2018-04-16 15:44:44 +0200

    Test max variation per plante < 100 during the whole simulation add helpers for inspection of light results

--- test/test_light_interception.py
+++ test/test_light_interception.py
@@ -36,7 +36,6 @@ def test_shift_in_light():
     # than 1000% (or x10)
     p = project.Project(name='shift_in_light')
     params = p.csv_parameters('sim_scheme_test.csv')[0]
-    params.update(dict(nbj=165))
     outs = p.which_outputs
     p.which_outputs = outs
     lsys, lstring = p.run(**params)
@@ -45,12 +44,15 @@ def test_shift_in_light():
     df['relative_Inc_PAR'] = df['Inc_PAR'] / df['Inc_PAR'].mean()
     df['relative_Sum_PAR'] = df['Sum_PAR'] / df['relative_Inc_PAR']
 
-    def _mean_variation(x):
+    def _max_variation(x):
         variation = (x.relative_Sum_PAR.diff().abs() / x.relative_Sum_PAR)[1:] * 100
-        return variation.mean()
+        return variation.max()
 
-    relative_variation = df.groupby(['Num_plante', 'Num_talle']).apply(_mean_variation)
-    assert all(relative_variation < 50)
+    # test max variation per plante
+    relative_variation = df.groupby(['Num_plante', 'Elapsed_time']).agg('sum').reset_index().groupby(
+        'Num_plante').apply(_max_variation)
+
+    assert all(relative_variation < 100)
     p.deactivate()
     p.remove(force=True)
 
@@ -72,43 +74,32 @@ def test_infinite():
     p.remove(force=True)
 
 
-def debug_stuff():
-    # some lines that can be run after lsys, lstring = p.run(**pars)
-    # to examine how caribu run
-    from openalea.lpy import Lsystem
-    from alinea.caribu.CaribuScene import CaribuScene
+# debug helper
 
-    fake_rule = """
-    Fake:
-      produce Fake"""
-    fake_lsys = Lsystem()
-    fake_lsys.addRule(fake_rule)
-
-    def force_cut(lstring):
-        fake_lsys.axiom = lstring
-        return fake_lsys.iterate()
-
-    lstring = force_cut(lstring)
+def get_res_sky(lsys, lstring):
+    """relaunch light simulation for the last time step"""
     lscene = lsys.sceneInterpretation(lstring)
     current_PAR = lsys.context().locals()['current_PAR']
     nb_azimuth = lsys.context().locals()['nb_azimuth']
     nb_zenith = lsys.context().locals()['nb_zenith']
-    light = get_light(current_PAR, nb_azimuth, nb_zenith)
-    c_scene = CaribuScene(scene=lscene, scene_unit="m", light=light)
-    _, res_sky = c_scene.run(simplify=True)
-    axis_census = lsys.context().locals()['axis_census']
-    Debug_PAR_dico_df = lsys.context().locals()['Debug_PAR_dico_df']
-    dico_PAR = lsys.context().locals()['dico_PAR']
-    tiller_surface = lsys.context().locals()['tiller_surface']
-    dico_PAR_per_axis = lsys.context().locals()['dico_PAR_per_axis']
-    Tempcum = lsys.context().locals()['Tempcum']
-    Temperature = lsys.context().locals()['Temperature']
+    crop_scheme = lsys.context().locals()['crop_scheme']
+    caribu_scene = lsys.context().locals()['caribu_scene']
 
-    # emulate endeach
-    if Temperature > 0 and res_sky:
-        for num_plt in axis_census.keys():
-            for axis in axis_census[num_plt].keys():
-                dico_PAR_per_axis[num_plt][axis][round(Tempcum, 1)] = 0
+    c_scene = caribu_scene(lscene, crop_scheme, current_PAR, nb_azimuth, nb_zenith)
+    _, res_sky = c_scene.run(simplify=True)
+    return res_sky
+
+
+def debug_par_dico_df(lsys, lstring, res_sky=None):
+    """Construct the Debug_PAR_dico_df"""
+    if res_sky is None:
+        res_sky = get_res_sky(lsys, lstring)
+    Debug_PAR_dico_df = {"Elapsed_time": [], "Temp_cum": [], "Num_plante": [], "Num_talle": [], "Organ_PAR": [],
+                         "Organ_type": [], "Num_organe": [], "Organ_surface": [], "Ei": [], "id": [], 'tiller_surface': []}
+    elapsed_time = lsys.context().locals()['elapsed_time']
+    axis_census = lsys.context().locals()['axis_census']
+    Temperature = lsys.context().locals()['Temperature']
+    tiller_surface = lsys.context().locals()['tiller_surface']
     for id in res_sky['Ei'].keys():
         # if id not in new_lstring: continue
         new_ = lstring[id]
@@ -116,23 +107,61 @@ def debug_stuff():
                 (new_.name in ("Sheath", "Internode", "Peduncle")) or
                 (new_.name == "Ear" and new_[0].emerged)):
             if new_[0].tiller in axis_census[new_[0].num_plante].keys():
+                Debug_PAR_dico_df["Elapsed_time"].append(elapsed_time)
+                Debug_PAR_dico_df["Temp_cum"].append(Temperature)
                 Debug_PAR_dico_df["Ei"].append(res_sky["Ei"][id])
+                Debug_PAR_dico_df["Organ_type"].append(lstring[id].name)
+                Debug_PAR_dico_df["id"].append(id)
+                Debug_PAR_dico_df["Num_talle"].append(lstring[id][0].tiller)
+                Debug_PAR_dico_df["Num_plante"].append(lstring[id][0].num_plante)
+                Debug_PAR_dico_df["Num_organe"].append(lstring[id][0].n)
+                Debug_PAR_dico_df["Organ_surface"].append(lstring[id][0].organ_surface)
+                Debug_PAR_dico_df["Organ_PAR"].append(lstring[id][0].PAR)
+                Debug_PAR_dico_df["tiller_surface"].append(tiller_surface[(lstring[id][0].num_plante,
+                                                                                          lstring[id][0].tiller)]*1e-4)
+    return Debug_PAR_dico_df
 
-                if res_sky["Ei"][id] < 0:
-                    new_[0].PAR = 0
+def shift_in_light_bug():
+    """Inspect the shift in light bug """
+    p = project.Project(name='shift_in_light')
+    params = p.csv_parameters('sim_scheme_test.csv')[0]
+    params.update(dict(nbj=225))
+    lsys, lstring = p.run(**params)
+    res_sky = get_res_sky(lsys, lstring)
+    Debug_PAR_dico_df = debug_par_dico_df(lsys, lstring, res_sky)
+    dd = pandas.DataFrame(Debug_PAR_dico_df)
+    dfag=dd.groupby(['Num_plante', 'Num_talle']).agg({'Organ_surface': 'sum', 'tiller_surface':'mean'})
+    dfag.tiller_surface / dfag.Organ_surface
+
+
+
+def debug_dico_PAR_per_axis(lsys, lstring, res_sky=None):
+    """generate a dico_par_per_axis like dict for the current time"""
+
+    axis_census = lsys.context().locals()['axis_census']
+    tiller_surface = lsys.context().locals()['tiller_surface']
+    Temperature = lsys.context().locals()['Temperature']
+    dico_PAR_per_axis = {}
+    for num_plt in axis_census.keys():
+        if num_plt not in dico_PAR_per_axis:
+            dico_PAR_per_axis[num_plt] = {}
+        for axis in axis_census[num_plt].keys():
+            dico_PAR_per_axis[num_plt][axis] = 0
+    for id in res_sky['Ei'].keys():
+        new_ = lstring[id]
+        if ((new_.name == "Blade" and new_[0].photosynthetic == True) or
+                (new_.name in ("Sheath", "Internode", "Peduncle")) or
+                (new_.name == "Ear" and new_[0].emerged)):
+            if new_[0].tiller in axis_census[new_[0].num_plante].keys():
+                if tiller_surface[(lstring[id][0].num_plante, lstring[id][0].tiller)] < 0.00001:
+                    dico_PAR_per_axis[lstring[id][0].num_plante][lstring[id][0].tiller] += 0
                 else:
-                    new_[0].PAR = res_sky["Ei"][id] * res_sky["area"][id]
+                    dico_PAR_per_axis[lstring[id][0].num_plante][lstring[id][0].tiller] += lstring[id][
+                                                                                              0].PAR \
+                                                                                           / Temperature / \
+                                                                                          tiller_surface[(
+                                                                                          lstring[id][0].num_plante,
+                                                                                          lstring[id][0].tiller)]
+    return dico_PAR_per_axis
 
-                new_[0].organ_surface = res_sky["area"][id]
-                dico_PAR[new_.name][new_[0].num_plante][new_[0].tiller][
-                    new_[0].n] = new_[0].PAR
 
-                if tiller_surface[(lstring[id][0].num_plante, lstring[id][
-                    0].tiller)] < 0.00001:
-                    dico_PAR_per_axis[lstring[id][0].num_plante][
-                        lstring[id][0].tiller][round(Tempcum, 1)] += 0
-                else:
-                    dico_PAR_per_axis[lstring[id][0].num_plante][
-                        lstring[id][0].tiller][round(Tempcum, 1)] += \
-                        lstring[id][0].PAR / Temperature / tiller_surface[
-                            (lstring[id][0].num_plante, lstring[id][0].tiller)]
