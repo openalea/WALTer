a2ed833d2668f2c9063c2928460eb63f1db7a7a8
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2018-04-17 18:05:06 +0200

    remove lpy cut bug management from WALTer.lpy keep testing if an error-prone version of lpy is installed

--- test/test_cut_module.py
+++ test/test_cut_module.py
@@ -4,23 +4,28 @@ from os.path import join as pj
 from openalea.lpy import Lsystem
 from walter import data_access
 
-fake_rule = """
-Fake:
-  produce Fake"""
+
+def force_cut(lstring):
+    fake_rule = """
+    Fake:
+      produce Fake"""
+    fake_lsys = Lsystem()
+    fake_lsys.addRule(fake_rule)
+    fake_lsys.axiom = lstring
+    return fake_lsys.iterate()
+
 
 def test_cut_bug():
     lsystem_file = pj(data_access.get_data_dir(), 'check_cut_module.lpy')
     lsys = Lsystem(lsystem_file)
     lstring = lsys.iterate()
     # cut branches
-    fake_lsys = Lsystem()
-    fake_lsys.addRule(fake_rule)
-    fake_lsys.axiom = lstring
-    new_lstring = fake_lsys.iterate()
+    new_lstring = force_cut(lstring)
     lscene = lsys.sceneInterpretation(lstring)
     for sid in lscene.todict():
         assert sid in range(len(lstring))
-        # suucceed if correction is needed
+        # suucceed if correction is needed (old lpy version)
         # assert new_lstring[sid].name == 'Internode'
-        #  succeed if lpy bug has been fixed
+        #  succeed on more recent lpy version
         assert lstring[sid].name == 'Internode'
+        # if correction is needed force_cut has to be applied in EndEach on lstring to get matching ids with lscene
