ad4466da08df959b41ee194e77fae0f2de191c29
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2018-04-24 19:42:18 +0200

    switch walter to consistent PAR per unit surface computing (use  actual visible primitive area instead of estimated visible area)

--- test/test_light_interception.py
+++ test/test_light_interception.py
@@ -1,5 +1,5 @@
 from walter import project
-from walter.light import get_light, scene_pattern
+from walter.light import scene_pattern
 import pandas
 
 
@@ -53,7 +53,7 @@ def test_shift_in_light():
     relative_variation = df.groupby(['Num_plante', 'Elapsed_time']).agg('sum').reset_index().groupby(
         'Num_plante').apply(_max_variation)
 
-    assert all(relative_variation < 100)
+    assert all(relative_variation < 50)
 
     p.deactivate()
     p.remove(force=True)
@@ -75,6 +75,7 @@ def test_infinite():
     p.deactivate()
     p.remove(force=True)
 
+# debug helper
 
 def check_light_balance():
     """test if total par intercepted is above or below incident par"""
@@ -92,8 +93,6 @@ def check_light_balance():
     p.remove(force=True)
 
 
-# debug helper
-
 def get_res_sky(lsys, lstring):
     """relaunch light simulation for the last time step"""
     lscene = lsys.sceneInterpretation(lstring)
@@ -108,37 +107,6 @@ def get_res_sky(lsys, lstring):
     return res_sky
 
 
-def debug_par_dico_df(lsys, lstring, res_sky=None):
-    """Construct the Debug_PAR_dico_df"""
-    if res_sky is None:
-        res_sky = get_res_sky(lsys, lstring)
-    Debug_PAR_dico_df = {"Elapsed_time": [], "Temp_cum": [], "Num_plante": [], "Num_talle": [], "Organ_PAR": [],
-                         "Organ_type": [], "Num_organe": [], "Organ_surface": [], "Ei": [], "id": [], 'tiller_surface': []}
-    elapsed_time = lsys.context().locals()['elapsed_time']
-    axis_census = lsys.context().locals()['axis_census']
-    Temperature = lsys.context().locals()['Temperature']
-    tiller_surface = lsys.context().locals()['tiller_surface']
-    for id in res_sky['Ei'].keys():
-        # if id not in new_lstring: continue
-        new_ = lstring[id]
-        if ((new_.name == "Blade" and new_[0].photosynthetic == True) or
-                (new_.name in ("Sheath", "Internode", "Peduncle")) or
-                (new_.name == "Ear" and new_[0].emerged)):
-            if new_[0].tiller in axis_census[new_[0].num_plante].keys():
-                Debug_PAR_dico_df["Elapsed_time"].append(elapsed_time)
-                Debug_PAR_dico_df["Temp_cum"].append(Temperature)
-                Debug_PAR_dico_df["Ei"].append(res_sky["Ei"][id])
-                Debug_PAR_dico_df["Organ_type"].append(lstring[id].name)
-                Debug_PAR_dico_df["id"].append(id)
-                Debug_PAR_dico_df["Num_talle"].append(lstring[id][0].tiller)
-                Debug_PAR_dico_df["Num_plante"].append(lstring[id][0].num_plante)
-                Debug_PAR_dico_df["Num_organe"].append(lstring[id][0].n)
-                Debug_PAR_dico_df["Organ_surface"].append(lstring[id][0].organ_surface)
-                Debug_PAR_dico_df["Organ_PAR"].append(lstring[id][0].PAR)
-                Debug_PAR_dico_df["tiller_surface"].append(tiller_surface[(lstring[id][0].num_plante,
-                                                                                          lstring[id][0].tiller)]*1e-4)
-    return Debug_PAR_dico_df
-
 def shift_in_light_bug():
     """Inspect the shift in light bug """
     p = project.Project(name='shift_in_light')
@@ -146,40 +114,8 @@ def shift_in_light_bug():
     params.update(dict(nbj=225))
     lsys, lstring = p.run(**params)
     res_sky = get_res_sky(lsys, lstring)
-    Debug_PAR_dico_df = debug_par_dico_df(lsys, lstring, res_sky)
-    dd = pandas.DataFrame(Debug_PAR_dico_df)
+    dd = pandas.DataFrame(lsys.context().locals()['PAR_per_organ'])
     dfag=dd.groupby(['Num_plante', 'Num_talle']).agg({'Organ_surface': 'sum', 'tiller_surface':'mean'})
     dfag.tiller_surface / dfag.Organ_surface
-
-
-
-def debug_dico_PAR_per_axis(lsys, lstring, res_sky=None):
-    """generate a dico_par_per_axis like dict for the current time"""
-
-    axis_census = lsys.context().locals()['axis_census']
-    tiller_surface = lsys.context().locals()['tiller_surface']
-    Temperature = lsys.context().locals()['Temperature']
-    dico_PAR_per_axis = {}
-    for num_plt in axis_census.keys():
-        if num_plt not in dico_PAR_per_axis:
-            dico_PAR_per_axis[num_plt] = {}
-        for axis in axis_census[num_plt].keys():
-            dico_PAR_per_axis[num_plt][axis] = 0
-    for id in res_sky['Ei'].keys():
-        new_ = lstring[id]
-        if ((new_.name == "Blade" and new_[0].photosynthetic == True) or
-                (new_.name in ("Sheath", "Internode", "Peduncle")) or
-                (new_.name == "Ear" and new_[0].emerged)):
-            if new_[0].tiller in axis_census[new_[0].num_plante].keys():
-                if tiller_surface[(lstring[id][0].num_plante, lstring[id][0].tiller)] < 0.00001:
-                    dico_PAR_per_axis[lstring[id][0].num_plante][lstring[id][0].tiller] += 0
-                else:
-                    dico_PAR_per_axis[lstring[id][0].num_plante][lstring[id][0].tiller] += lstring[id][
-                                                                                              0].PAR \
-                                                                                           / Temperature / \
-                                                                                          tiller_surface[(
-                                                                                          lstring[id][0].num_plante,
-                                                                                          lstring[id][0].tiller)]
-    return dico_PAR_per_axis
 
 
