cd1ff5ef448f68a0dfe7fe68d60a5b4534f7a511
bilalderouich <bilalderouich34@gmail.com>
2018-06-20 14:23:04 +0200

    repare global test

--- test/test_no_regression.py
+++ test/test_no_regression.py
@@ -10,12 +10,8 @@ def test_same_result():
 
     p = project.Project(name='same') # Create the simulation directory
     params = p.csv_parameters('sim_scheme_test.csv')[0] # recovery of parameters
-    params.update(dict(nbj=325)) #Adding parameters to vary
-    outs = p.which_outputs #Create the output file if it doesn't exists
-    p.which_outputs = outs #Assign the output file to the test output to save the results
     lsys, lstring = p.run(**params)
-    with open(p.dirname/'ID_simul.txt') as f:
-        id = f.read() # Recovery of file
+    result_directory = str(p.output_path()) + '/'
     reference_directory = get_data_dir() + "/ref_output/" # Reference folder
     list_of_file_ref = os.listdir(reference_directory) # Listing of the different reference files
     reference = {}
@@ -23,7 +19,6 @@ def test_same_result():
         element = reference_directory + i # Recover the absolut path
         up_date = { i : pandas.read_csv(element, sep='\t')} # Recover file and content
         reference.update(up_date) # Reference dictionnary
-        result_directory = p.dirname + '/' + 'output' + '/' + id +'/'
         list_of_result = os.listdir(result_directory)
         np.assert_array_equal(list_of_file_ref,list_of_result) # Check that the 2 lists are equal
         my_file = Path(result_directory + i)
@@ -35,6 +30,5 @@ def test_same_result():
             print(' \n The ' + my_file + ' file is non-existent ')
 
 
-    p.deactivate()
     p.remove(force=True)
 
