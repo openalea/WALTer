7182ac4079b1f354102b62a8f909b8eacd2aa2b0
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2018-10-12 17:15:18 +0200

    add try/except raise/ finally error catch blocks in test to ensure test independence and cleanup of test project directories

--- test/test_light_interception.py
+++ test/test_light_interception.py
@@ -8,19 +8,22 @@ def test_zero_light():
     #assert : all tillers receive light (value > 0 for Sum_PAR in the PAR_per_axes.csv output file)
 
     p = project.Project(name='zero_light') #Create Folder
-    params = p.csv_parameters('sim_scheme_test.csv')[0]  #Recover list of parameters
-    params.update(dict(nb_plt_temp=50, nb_rang=10, nbj=156)) #Add new parameters
-    lsys, lstring = p.run(**params)
-    PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']
-    df = pandas.DataFrame(PAR_per_axes_dico)
-    PAR = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
-    assert all(PAR > 0)
-    outdir = p.output_path()
-    dfout = pandas.read_csv(outdir/'PAR_per_axes.csv', sep='\t')
-    PARout = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
-    assert all(PARout > 0)
-
-    p.remove(force=True)
+    try:
+        params = p.csv_parameters('sim_scheme_test.csv')[0]  #Recover list of parameters
+        params.update(dict(nb_plt_temp=50, nb_rang=10, nbj=156)) #Add new parameters
+        lsys, lstring = p.run(**params)
+        PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']
+        df = pandas.DataFrame(PAR_per_axes_dico)
+        PAR = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
+        assert all(PAR > 0)
+        outdir = p.output_path()
+        dfout = pandas.read_csv(outdir/'PAR_per_axes.csv', sep='\t')
+        PARout = df.groupby('Num_plante').agg('sum')['Sum_PAR'].values
+        assert all(PARout > 0)
+    except:
+        raise
+    finally:
+        p.remove(force=True)
 
 
 def test_shift_in_light():
@@ -31,40 +34,47 @@ def test_shift_in_light():
     # (PAR_per_axes.csv output file ; value for Sum_PAR) is always less
     # than 1000% (or x10)
     p = project.Project(name='shift_in_light')
-    # run the reference simulation
-    lsys, lstring = p.run_parameters('sim_scheme_test.csv')
-    PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']
-    df = pandas.DataFrame(PAR_per_axes_dico)
-    df['relative_Inc_PAR'] = df['Inc_PAR'] / df['Inc_PAR'].mean()
-    df['relative_Sum_PAR'] = df['Sum_PAR'] / df['relative_Inc_PAR']
+    try:
+        # run the reference simulation
+        lsys, lstring = p.run_parameters('sim_scheme_test.csv')
+        PAR_per_axes_dico = lsys.context().locals()['PAR_per_axes_dico']
+        df = pandas.DataFrame(PAR_per_axes_dico)
+        df['relative_Inc_PAR'] = df['Inc_PAR'] / df['Inc_PAR'].mean()
+        df['relative_Sum_PAR'] = df['Sum_PAR'] / df['relative_Inc_PAR']
 
-    def _max_variation(x):
-        variation = (x.relative_Sum_PAR.diff().abs() / x.relative_Sum_PAR)[1:] * 100
-        return variation.max()
+        def _max_variation(x):
+            variation = (x.relative_Sum_PAR.diff().abs() / x.relative_Sum_PAR)[1:] * 100
+            return variation.max()
 
-    # test max variation per plante
-    relative_variation = df.groupby(['Num_plante', 'Elapsed_time']).agg('sum').reset_index().groupby(
-        'Num_plante').apply(_max_variation)
+        # test max variation per plante
+        relative_variation = df.groupby(['Num_plante', 'Elapsed_time']).agg('sum').reset_index().groupby(
+            'Num_plante').apply(_max_variation)
 
-    assert all(relative_variation < 50)
-
-    p.remove(force=True)
+        assert all(relative_variation < 50)
+    except:
+        raise
+    finally:
+        p.remove(force=True)
 
 
 def test_infinite():
     p = project.Project(name='infinite_canopy')
-    params = p.csv_parameters('sim_scheme_test.csv')[0]
-    params.update(dict(nb_plt_utiles=1,
-                          dist_border_x=0,
-                          dist_border_y=0,
-                          nbj=53,
-                          infinity_CARIBU=1,
-                          beginning_CARIBU=290))
-    lsys, lstring = p.run(**params)
-    crop_scheme = lsys.context().locals()['crop_scheme']
-    pattern = scene_pattern(crop_scheme)
-    assert pattern[0] > 1
-    p.remove(force=True)
+    try:
+        params = p.csv_parameters('sim_scheme_test.csv')[0]
+        params.update(dict(nb_plt_utiles=1,
+                              dist_border_x=0,
+                              dist_border_y=0,
+                              nbj=53,
+                              infinity_CARIBU=1,
+                              beginning_CARIBU=290))
+        lsys, lstring = p.run(**params)
+        crop_scheme = lsys.context().locals()['crop_scheme']
+        pattern = scene_pattern(crop_scheme)
+        assert pattern[0] > 1
+    except:
+        raise
+    finally:
+        p.remove(force=True)
 
 # debug helper
 
