c9b710be3adb38b9aca19a7a5d5e70ce375c2a09
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2019-04-10 15:09:58 +0200

    remove unnecessary copy or read_itable code

--- test/test_command_line.py
+++ test/test_command_line.py
@@ -1,27 +1,14 @@
 import os
 from shutil import rmtree, copy
 from walter.command_line import walter_parser
-import json
+from walter.project import Project
+
 
 call_dir = os.getcwd()
 
 
 def reset_call_dir():
     os.chdir(call_dir)
-
-def read_itable(path):
-    def _byteify(input):
-        if isinstance(input, dict):
-            return {_byteify(key): _byteify(value) for key, value in input.iteritems()}
-        elif isinstance(input, list):
-            return [_byteify(element) for element in input]
-        elif isinstance(input, unicode):
-            return input.encode('utf-8')
-        else:
-            return input
-# The use of the byteify function kill encoding problems from json importation between unicode and strings
-    with open(path) as itable:
-        return _byteify(json.load(itable))
 
 
 def test_parser():
@@ -58,12 +45,14 @@ def test_multi_simulation():
     if os.path.exists('test_multi_sim'):
         rmtree('test_multi_sim')
 
+
 def test_combi_params_error():
+    """Write the test here because detect a double prec read bug that need re-read of index-table"""
     reset_call_dir()
     cmd = "walter -i sim_scheme_test_combi_params_err.csv -p combi_params_error"
     os.system(cmd)
     reset_call_dir()
-    index_table_test = read_itable("combi_params_error/index-table.json")
+    index_table_test = Project.read_itable("combi_params_error/index-table.json")
     if os.path.exists('combi_params_error'):
         rmtree('combi_params_error')
     assert len(index_table_test) == 2
