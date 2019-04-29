615813f0ea515c29d8de56aa23cecc3d164331c0
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2019-04-29 15:59:40 +0200

    improve non regression test

--- test/test_no_regression.py
+++ test/test_no_regression.py
@@ -6,11 +6,40 @@ from walter.data_access import get_data_dir
 from pathlib2 import Path
 import numpy.testing as np
 import re
+from time import sleep
 
 _rtol_PAR = 0.02 # windows and linux version of walter lead up to 2 percent PAR output difference
 
 
-def test_same_result(keep_results=False):
+def _compare_list_of_files(p):
+    result_directory = str(p.output_path()) + os.sep
+    reference_directory = os.path.join(get_data_dir(), "ref_output", '')  # Reference folder
+    list_of_file_ref = map(os.path.basename, glob.glob(reference_directory + '*.csv'))  # Listing of the different reference files
+    list_of_result = map(os.path.basename, glob.glob(result_directory + '*.csv'))
+    np.assert_array_equal(list_of_file_ref, list_of_result) # Check that the 2 lists are equal
+
+def _compare_file_content(p, rtol=_rtol_PAR):
+    result_directory = str(p.output_path()) + os.sep
+    reference_directory = os.path.join(get_data_dir(), "ref_output", '')  # Reference folder
+    list_of_file_ref = map(os.path.basename, glob.glob(reference_directory + '*.csv'))  # Listing of the different reference files
+    for i in list_of_file_ref:
+        element = reference_directory + i  # Recover the absolut path
+        reference = pandas.read_csv(element, sep='\t')  # Recover file and content
+        my_file = Path(result_directory + i)
+        if my_file.is_file():
+            dfout = pandas.read_csv(my_file, sep='\t')
+            print(' \n The tested file is : ' + i + '\n')
+            # pandas.testing.assert_frame_equal(reference, dfout)
+            for column in reference.columns:
+                # print column
+                if re.match('.*PAR.*', column):
+                    np.assert_allclose(dfout[column], reference[column], rtol=rtol)
+                else:
+                    pandas.testing.assert_series_equal(dfout[column], reference[column])
+        else:
+            print(' \n The ' + my_file + ' file is non-existent ')
+
+def test_same_result(debug=False):
     #assert : verify if the same wheat field have the same results for the same chosen parameters.
 
     p = project.Project(name='same') # Create the simulation directory
@@ -18,29 +47,23 @@ def test_same_result(keep_results=False):
         directory = project.walter_data()
         params = p.csv_parameters(str(directory/'sim_scheme_ref.csv'))[0] # recovery of parameters
         p.run(**params)
-        result_directory = str(p.output_path()) + '/'
-        reference_directory = get_data_dir() + "/ref_output/" # Reference folder
-        list_of_file_ref = glob.glob(reference_directory + '*.csv') # Listing of the different reference files
-        list_of_result = glob.glob(result_directory + '*.csv')
-        np.assert_array_equal(list_of_file_ref, list_of_result)  # Check that the 2 lists are equal
+        try:
+            _compare_list_of_files(p)
+        except AssertionError: #on windows the test is executed before walter had time to write results
+            sleep(5)
+            _compare_list_of_files(p)
 
-        for i in list_of_file_ref:
-            element = reference_directory + i # Recover the absolut path
-            reference = pandas.read_csv(element, sep='\t') # Recover file and content
-            my_file = Path(result_directory + i)
-            if my_file.is_file():
-                dfout = pandas.read_csv(my_file, sep='\t')
-                print(' \n The tested file is : '+ i + '\n')
-                for column in reference.columns:
-                    if re.match('.*PAR.*', column):
-                        np.assert_allclose(dfout[column], reference[column], rtol=_rtol_PAR)
-                    else:
-                        np.assert_array_equal(dfout[column], reference[column])
-            else:
-                print(' \n The ' + my_file + ' file is non-existent ')
+        try:
+            _compare_file_content(p)
+        except AssertionError:
+            sleep(5)
+            _compare_file_content(p)
     except:
         raise
     finally:
-        if not keep_results:
+        if debug:
+            return p
+        else:
             p.remove(force=True)
+
 
