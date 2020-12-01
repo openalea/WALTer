69cf07ef53dbe1bc34d49e6692e446924b9e3120
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2020-12-01 16:05:49 +0100

    add spec of geometry_analysis

---
+++ src/walter/geometry_analysis.py
@@ -0,0 +1,95 @@
+"""
+Specification
+"""
+
+def load_lstring_lscene(output_dir, date):
+    """reload lstring and lscene from walter output dir"""
+
+def structured_scene(lstring, lscene):
+    """
+
+    Parameters
+    ----------
+    lstring
+    lscene
+
+    Returns
+    -------
+     mtg or equivalentpandas that tag geometry with label of different elements
+    """
+    pass
+
+def query(scene, plant=None, genotype=None, organ=None):
+    """
+
+    Parameters
+    ----------
+    scene
+    plant
+    genotype
+    organ
+
+    Returns
+    -------
+        a scene corresponding to the selection
+    """
+
+###############################################################################
+
+
+def proj_surface(scene, direction=None):
+    """
+
+    Parameters
+    ----------
+    geometry
+    direction
+
+    Returns
+    -------
+        projected surface per structure (int)
+    """
+
+def surface(geometry):
+    """
+
+    Parameters
+    ----------
+    geometry
+
+    Returns
+    -------
+
+    """
+
+def gap_fraction():
+    """
+    """
+
+def dominance_propability():
+    """ may be mutual_shading"""
+
+def bounding_cylinder(geometry):
+    """
+
+    Parameters
+    ----------
+    geometry
+
+    Returns
+    -------
+
+    """
+
+
+def equivalent_cylinder(geometry):
+    """A cylinder that has equivalent property of geometry, witto interseption
+
+    Parameters
+    ----------
+    geometry
+
+    Returns
+    -------
+
+    """
