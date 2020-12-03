06f4d48a1cc30ae26e3c81ade780e0ce9a7cd62b
pradal <christophe.pradal@inria.fr>
2020-12-03 10:26:27 +0100

    fisrts test for geometry

---
+++ test/test_geometry_analysis.py
@@ -0,0 +1,29 @@
+from walter import project
+from walter.light import scene_pattern
+from openalea.plantgl.all import Scene
+
+def test1():
+    p = project.Project(name='zero_light')
+    params = p.csv_parameters('sim_scheme_fast_test.csv')[0]
+    params.update(dict(nbj=100, ))
+    p.which_outputs = dict(Lstring= 1, Lscene=1)
+    lsys, lstring = p.run(**params)
+    lscene = lsys.sceneInterpretation(lstring)
+    p.deactivate()
+    return p, lstring, lscene
+
+def save_all(lstring, lscene, prefix='test1'):
+    s_save = Scene(lscene)
+    s_save.save('%s.bgeom'%prefix)
+
+    with open("%s_lstring.txt" % prefix, "w") as out_lstring:
+        out_lstring.write("%s" % lstring)
+    
+def read_all(prefix='test1'):
+    from openalea.lpy import Lstring
+    lscene = Scene('%s.bgeom'%prefix)
+    with open("%s_lstring.txt" % prefix, "r") as in_lstring:
+        lstring = in_lstring.read()
+
+    lstring = Lstring(lstring)
+    return lscene, lstring
