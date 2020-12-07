f6c3250253afb3da639e33d042c1864efffa6d9a
Meije <meije.gawinowski@inra.fr>
2020-12-07 08:48:44 +0100

    add changes to test_geometry_caribu to specify directory and write files

--- test/test_geometry_analysis_caribu.py
+++ test/test_geometry_analysis_caribu.py
@@ -1,23 +1,25 @@
 from walter import project
 import walter.geometry_analysis_caribu as gac
+import os
 
 
 
-
-def test1(nbj=30):
-    p = project.Project()
+def test1(dirname, name, nbj=30):
+    os.chdir(dirname)
+    p = project.Project(name)
     lsys, lstring = p.run(nb_plt_utiles=2,
                           dist_border_x=0,
                           dist_border_y=0,
                           nbj=nbj,
                           beginning_CARIBU=290)
     lscene = lsys.sceneInterpretation(lstring)
-    p.remove(force=True)
+    #p.remove(force=True)
     return p, lsys, lstring, lscene
 
 
-def test_analyse(nbj=30):
-    p, lsys, lstring, lscene = test1(nbj)
+def test_analyse(dirname, name, nbj=30):
+    os.chdir(dirname)
+    p, lsys, lstring, lscene = test1(dirname, name, nbj)
     crop_scheme = lsys.context().locals()['crop_scheme']
     plant_map = lsys.context().locals()['plant_map']
     df = gac.analyse(lscene, lstring, crop_scheme, plant_map)
